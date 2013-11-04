f585397a13fcbca8093ffd7993f94d5fddf50fc0
mytoh <mytoh@gmail.com>
2013-11-05 07:12:42 +0900

    remove unnecessary requirements

--- lib/pikkukivi/commands/verkko/futaba.scm
+++ lib/pikkukivi/commands/verkko/futaba.scm
@@ -15,7 +15,6 @@
   (use kirjasto.merkkijono)
   (use kirjasto.pääte)
   (use maali)
-  (use clojure)
   (require-extension (srfi 1))
   )
 (select-module pikkukivi.commands.verkko.futaba)
@@ -24,53 +23,53 @@
 
 (define (usage)
   (print-strings
-    '("Usage: futaba board thread"
-      "  option:"
-      "\t-a|all      get thread number from directories under cwd"
-      "\t-r|repeat   repeat script with interval 5 minutes"
-      "\tboard      only supports b,k,l,7,40"
-      "\tthread      3839 2230 93988 482208 ..."
-      "  expamle: "
-      "\t$ futaba b 222222         # get images once from /b/222222 "
-      "\t$ futaba -r id 9999       # get images from /id/9999 with repeat option"
-      "\t$ futaba -a b             # get images from b with directory name as thread number"))
+   '("Usage: futaba board thread"
+     "  option:"
+     "\t-a|all      get thread number from directories under cwd"
+     "\t-r|repeat   repeat script with interval 5 minutes"
+     "\tboard      only supports b,k,l,7,40"
+     "\tthread      3839 2230 93988 482208 ..."
+     "  expamle: "
+     "\t$ futaba b 222222         # get images once from /b/222222 "
+     "\t$ futaba -r id 9999       # get images from /id/9999 with repeat option"
+     "\t$ futaba -a b             # get images from b with directory name as thread number"))
   (exit 2))
 
 
 (define (parse-img-url line board)
   (rxmatch->string
-    (string->regexp
-      (str
+   (string->regexp
+    (string-append
         "http\:\/\/(\\w+)\\.2chan\\.net\/(\\w+)\/"
-        board
-        "\/src\/[^\"]+"))
-    line))
+      board
+      "\/src\/[^\"]+"))
+   line))
 
 
 (define (get-img html board)
   (let ((image-url-list (remove not
-                                (call-with-input-string html
-                                  (lambda (in)
-                                    (port-map
-                                      (lambda (line)
-                                        (let ((m (parse-img-url line board)))
-                                          m))
-                                      (cut read-line in #t)))))))
+                          (call-with-input-string html
+                                                  (lambda (in)
+                                                    (port-map
+                                                     (lambda (line)
+                                                       (let ((m (parse-img-url line board)))
+                                                         m))
+                                                     (cut read-line in #t)))))))
     (flush)
     (let ((got-images (remove not
-                              (map (lambda (url) (fetch url))
-                                   image-url-list))))
+                        (map (lambda (url) (fetch url))
+                          image-url-list))))
       (match (length got-images)
         (0 (newline))
-        (1 (print (str " " (paint (number->string (length got-images)) 49)
-                       " file")))
-        (_ (print (str " " (paint (number->string (length got-images)) 49)
-                       " files")))))))
+        (1 (print (string-append " " (paint (number->string (length got-images)) 49)
+                                 " file")))
+        (_ (print (string-append " " (paint (number->string (length got-images)) 49)
+                                 " files")))))))
 
 (define (url->filename url)
   (receive (a fname ext)
-    (decompose-path (values-ref (uri-parse url) 4))
-    (path-swap-extension fname ext)))
+           (decompose-path (values-ref (uri-parse url) 4))
+           (path-swap-extension fname ext)))
 
 (define (fetch uri)
   (when (string? uri)
@@ -80,11 +79,11 @@
              (flusher (lambda (sink headers)  #t)))
         (if (not (file-is-readable? file))
           (receive (temp-out temp-file)
-            (sys-mkstemp "yotsuba-temp")
-            (http-get hostname path
-                      :sink temp-out :flusher flusher)
-            (close-output-port temp-out)
-            (move-file temp-file file))
+                   (sys-mkstemp "yotsuba-temp")
+                   (http-get hostname path
+                             :sink temp-out :flusher flusher)
+                   (close-output-port temp-out)
+                   (move-file temp-file file))
           #f)))))
 
 
@@ -93,8 +92,8 @@
   (let-values (((status headers body)
                 (let* ((bd board)
                        (fget (lambda (server)
-                               (http-get (str server ".2chan.net")
-                                         (str "/" board "/res/" td ".htm")))))
+                               (http-get (string-append server ".2chan.net")
+                                         (string-append "/" board "/res/" td ".htm")))))
                   (match bd
                     ("l" ;二次元壁紙
                      (fget  "dat" ))
@@ -104,18 +103,18 @@
                      (let ((servs '("jun" "dec" "may"))
                            (get-res (lambda (srv)
                                       (receive (a b c)
-                                        (fget srv)
-                                        (if-not (string=? a "404") srv #f))))
+                                               (fget srv)
+                                               (if (not (string=? a "404")) srv #f))))
                            (get-values (lambda (srv)
                                          (receive (a b c)
-                                           (fget srv)
-                                           (when (not (string=? a "404")) (values a b c))))))
+                                                  (fget srv)
+                                                  (when (not (string=? a "404")) (values a b c))))))
                        (or (and-let* ((s (get-res "jun")))
-                             (get-values "jun"))
+                                     (get-values "jun"))
                          (and-let* ((s (get-res "dec")))
-                           (get-values "dec"))
+                                   (get-values "dec"))
                          (and-let* ((s (get-res "may")))
-                           (get-values "may"))
+                                   (get-values "may"))
                          (values "404" #f #f))))
                     ("7" ;ゆり
                      (fget "zip"))
@@ -126,7 +125,7 @@
              (if (string-incomplete? html)
                (string-incomplete->complete html :omit)
                html)))
-      (else  #f))))
+          (else  #f))))
 
 (define (futaba-get args)
   (let* ((board (car args))
@@ -140,7 +139,7 @@
        (get-img html board)
        (cd ".."))
       (else
-        (print (paint (str thread "'s gone") 237))))))
+          (print (paint (string-append thread "'s gone") 237))))))
 
 (define (futaba-get-all args)
   (let ((board (car args))
@@ -148,11 +147,11 @@
     (cond
       ((not (null? dirs))
        (for-each
-         (lambda (d)
-           (futaba-get (list board d)))
+           (lambda (d)
+             (futaba-get (list board d)))
          dirs)
-       (run-process `(notify-send ,(str "futaba " board  " fetch finished"))))
-      (else  (print "no directories")))))
+       (run-process `(notify-send ,(string-append "futaba " board  " fetch finished"))))
+      (else (print "no directories")))))
 
 (define (futaba-get-repeat args)
   (let* ((board (car args))
@@ -167,7 +166,7 @@
        (get-img html board)
        (cd ".."))
       (else
-        (display (paint (str thread "'s gone") 237))
+          (display (paint (string-append thread "'s gone") 237))
         (flush)
         (sys-select #f #f #f 100000)
         (display "\r")
@@ -175,33 +174,33 @@
 
 (define (futaba-get-repeat-all args)
   (loop-forever
-    (let ((board (car args))
-          (dirs (values-ref (directory-list2 (current-directory) :children? #t) 0)))
-      (print (str "Board " (paint board 229 )))
-      (cond
-        ((not (null? dirs))
-         (for-each
-           (lambda (d)
-             (futaba-get-repeat (list board d)))
-           dirs))
-        (else (print "no directories")))
-      (print (paint "----------" 237)))))
+   (let ((board (car args))
+         (dirs (values-ref (directory-list2 (current-directory) :children? #t) 0)))
+     (print (string-append "Board " (paint board 229)))
+     (cond
+       ((not (null? dirs))
+        (for-each
+            (lambda (d)
+              (futaba-get-repeat (list board d)))
+          dirs))
+       (else (print "no directories")))
+     (print (paint "----------" 237)))))
 
 (define (futaba args)
   (let-args args
-    ((all "a|all")
-     (repeat "r|repeat")
-     (else (opt . _) (print "Unknown option: " opt) (usage))
-     . restargs)
-    (cond
-      ((null? restargs)
-       (usage))
-      ((and all repeat)
-       (futaba-get-repeat-all restargs))
-      (repeat
-        (loop-forever
-          (futaba-get-repeat restargs)))
-      (all
-        (futaba-get-all restargs))
-      (else
-        (futaba-get restargs)))))
+            ((all "a|all")
+             (repeat "r|repeat")
+             (else (opt . _) (print "Unknown option: " opt) (usage))
+             . restargs)
+            (cond
+              ((null? restargs)
+               (usage))
+              ((and all repeat)
+               (futaba-get-repeat-all restargs))
+              (repeat
+               (loop-forever
+                (futaba-get-repeat restargs)))
+              (all
+               (futaba-get-all restargs))
+              (else
+                  (futaba-get restargs)))))
