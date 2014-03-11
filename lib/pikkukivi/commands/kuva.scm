337c9f6b3412d433badb9ad0d5e28d817453acc3
mytoh <mytoh@gmail.com>
2014-03-12 08:54:43 +0900

    * lib/pikkukivi/commands/kuva.scm: add file open functions

--- lib/pikkukivi/commands/kuva.scm
+++ lib/pikkukivi/commands/kuva.scm
@@ -1,6 +1,5 @@
 
-
-(define-library (pikkukivi commands kuva)
+ (define-library (pikkukivi commands kuva)
     (export
       kuva)
   (import
@@ -16,7 +15,28 @@
   (begin
     (define (usage status) (exit status "usage: ~a <file>\n" *program-name*))
 
-    (define feh
+    (define (open-directory dir)
+      (run-process `(feh "-F" ,dir) :wait #t))
+
+    (define (open-regular-file file)
+      (run-process `(feh -Z -F  -q --start-at
+                         ,(sys-realpath file)
+                         ,(sys-dirname (sys-realpath file)))
+                   :wait #t))
+
+    (define (open-archive file)
+      (let ((temp (build-path
+                   (temporary-directory)
+                   "kuva"
+                   (string-incomplete->complete
+                    (sys-basename
+                     (path-sans-extension file))))))
+        (make-directory* temp)
+        (unpack (list file temp))
+        (run-process `(feh -F -Z -r -q ,temp) :wait #t)
+        (remove-directory* temp)))
+
+    (define open
       (lambda (args)
         (cond ((null? args)
                (run-process `(feh) :wait #t))
@@ -24,24 +44,12 @@
                   (let-args args ((#f "h|help" (usage 0)) . rest)
                             (match (car rest)
                                    ((? file-is-directory? dir)
-                                    (run-process `(feh "-F" ,dir)) :wait #t)
+                                    (open-directory dir))
                                    ((? file-is-archive? file)
-                                    (let ((temp (build-path
-                                                 (temporary-directory)
-                                                 "kuva"
-                                                 (string-incomplete->complete
-                                                  (sys-basename
-                                                   (path-sans-extension file))))))
-                                      (make-directory* temp)
-                                      (unpack (list file temp))
-                                      (run-process `(feh -F -Z -r -q ,temp) :wait #t)
-                                      (remove-directory* temp)))
+                                    (open-archive file))
                                    ((? file-is-regular? file)
-                                    (run-process `(feh -Z -F  -q --start-at
-                                                       ,(sys-realpath file)
-                                                       ,(sys-dirname (sys-realpath file)))
-                                                 :wait #t))
+                                    (open-regular-file file))
                                    (_ (usage 1))))))))
 
     (define (kuva args)
-      (feh args))))
+      (open args))))
