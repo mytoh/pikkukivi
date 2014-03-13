e349b6fe2b90bdc349458f23c10d47be1b530d15
mytoh <mytoh@gmail.com>
2014-03-13 14:44:53 +0900

    * lib/pikkukivi/commands/unpack.scm: rewrite

--- lib/pikkukivi/commands/unpack.scm
+++ lib/pikkukivi/commands/unpack.scm
@@ -3,72 +3,74 @@
 ;;; cv2tar.scm by
 ;;;  Walter C. Pelissero
 
-(define-module pikkukivi.commands.unpack
-  (export unpack)
-  (use gauche.process)
-  (use file.util) ; path-extension
-  )
-(select-module pikkukivi.commands.unpack)
+(define-library (pikkukivi commands unpack)
+    (export unpack)
+  (import
+    (scheme base)
+    (gauche base)
+    (gauche process)
+    (file util) ; path-extension
+    )
+  (begin
+
+    (define (zip-unpacker file . directory)
+      (if (null? (car directory))
+        (run-process `(unzip -q ,file) :wait #t)
+        (run-process `(unzip -q ,file -d ,(caar directory)) :wait #t)))
 
 
-(define (zip-unpacker file . directory)
-  (if (null? (car directory))
-    (run-process `(unzip -q ,file) :wait #t)
-    (run-process `(unzip -q ,file -d ,(caar directory)) :wait #t)))
+    (define (rar-unpacker file . directory)
+      (cond ((null? (car directory))
+             (run-process `(unrar x -ad -inul ,file) :wait #t))
+            (else
+                (run-process `(unrar x -ad -inul ,file ,(caar directory)) :wait #t))))
 
+    (define (lha-unpacker file . diretory)
+      (run-process `(lha xq ,file) :wait #t))
 
-(define (rar-unpacker file . directory)
-  (cond ((null? (car directory))
-         (run-process `(unrar x -ad -inul ,file) :wait #t))
-    (else
-      (run-process `(unrar x -ad -inul ,file ,(caar directory)) :wait #t))))
+    (define (tar-unpacker file . directory)
+      (cond ((null? (car directory))
+             (run-process `(tar xf ,file) :wait #t))
+            (else
+                (make-directory* (caar directory))
+              (run-process `(tar xf ,file -C ,(caar directory)) :wait #t))))
 
-(define (lha-unpacker file . diretory)
-  (run-process `(lha xq ,file) :wait #t))
+    (define (sevenzip-unpacker file . directory)
+      (if (null? (car directory))
+        (run-process `(7z x ,file) :wait #t)
+        (run-process `(7z x ,file -o ,(caar directory)) :wait #t)))
 
-(define (tar-unpacker file . directory)
-  (cond ((null? (car directory))
-         (run-process `(tar xf ,file) :wait #t))
-    (else
-      (make-directory* (caar directory))
-      (run-process `(tar xf ,file -C ,(caar directory)) :wait #t))))
+    (define-constant *unpacker-alist*
+      `(
+        ("zip" ,zip-unpacker)
+        ("cbz" ,zip-unpacker)
 
-(define (sevenzip-unpacker file . directory)
-  (if (null? (car directory) )
-    (run-process `(7z x ,file) :wait #t)
-    (run-process `(7z x ,file -o ,(caar directory)) :wait #t)))
+        ("7z" ,sevenzip-unpacker)
 
-(define-constant *unpacker-alist*
-  `(
-    ("zip" ,zip-unpacker)
-    ("cbz" ,zip-unpacker)
+        ("rar" ,rar-unpacker)
+        ("cbr" ,rar-unpacker)
 
-    ("7z" ,sevenzip-unpacker)
+        ("lha" ,lha-unpacker)
 
-    ("rar" ,rar-unpacker)
-    ("cbr" ,rar-unpacker)
+        ("gz"  ,tar-unpacker)
+        ("tgz" ,tar-unpacker)
+        ("bz2" ,tar-unpacker)
+        ("xz"  ,tar-unpacker)
+        ("txz" ,tar-unpacker)
+        ("cbx" ,tar-unpacker)
+        ("tar" ,tar-unpacker)))
 
-    ("lha" ,lha-unpacker)
+    (define (unpacker file . directory)
+      (let ((unpacker (cadr (assoc (path-extension file)
+                              *unpacker-alist*))))
+        (print file)
+        (if unpacker
+          (if directory
+            (unpacker file directory)
+            (unpacker file))
+          (error "unknown file type" file))))
 
-    ("gz"  ,tar-unpacker)
-    ("tgz" ,tar-unpacker)
-    ("bz2" ,tar-unpacker)
-    ("xz"  ,tar-unpacker)
-    ("txz" ,tar-unpacker)
-    ("cbx" ,tar-unpacker)
-    ("tar" ,tar-unpacker)))
-
-(define (unpacker file . directory)
-  (let ((unpacker (cadr (assoc (path-extension file)
-                         *unpacker-alist*))))
-    (print file)
-    (if unpacker
-      (if directory
-        (unpacker file directory)
-        (unpacker file))
-      (error "unknown file type" file))))
-
-(define (unpack args)
-  (if (<= 2 (length args))
-    (unpacker (car args) (cadr args))
-    (unpacker (car args))))
+    (define (unpack args)
+      (if (<= 2 (length args))
+        (unpacker (car args) (cadr args))
+        (unpacker (car args))))))
