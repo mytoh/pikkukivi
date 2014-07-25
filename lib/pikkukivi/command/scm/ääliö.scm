14fa1d19a35eb2d7d6b5cdd682b9aa0600e40d67
mytoh <mytoh@gmail.com>
2014-07-26 08:30:56 +0900

    * lib/pikkukivi/command/scm/ääliö.scm: remove clone function
    remove static repository list and function to clone repository
    from its list.

--- lib/pikkukivi/command/scm/ääliö.scm
+++ lib/pikkukivi/command/scm/ääliö.scm
@@ -21,105 +21,6 @@
 
     (define *gitdir*  (expand-path "~/huone/git/"))
 
-    (define *repos*
-      '(; normal repo
-        "git://gitorious.org/cmus/cmus.git"
-        "git://git.sv.gnu.org/screen.git"
-        "git://git.savannah.gnu.org/stow.git"
-        "git://git.infradead.org/get_iplayer.git"
-        "git://git.mplayer2.org/mplayer2.git"
-        "git://git.informatik.uni-erlangen.de/re06huxa/herbstluftwm"
-        "git://opensource.conformal.com/xxxterm.git"
-        "git://tron.homeunix.org/cvscvt"
-        "git://pcmanfm.git.sourceforge.net/gitroot/pcmanfm/libfm"
-        "git://pcmanfm.git.sourceforge.net/gitroot/pcmanfm/pcmanfm"
-        ;; repo with other name
-        ("git://git.sourceforge.jp/gitroot/ninix-aya/master.git"  "ninix-aya")
-        ;; github repo
-        (okuoku        mosh)
-        (tmbinc        bgrep)
-        (ninjaaron     bitocra)
-        (koron         chalice)
-        (rossy2401     img2xterm)
-        (Arrowmaster   mcomix)
-        (robbyrussell  oh-my-zsh)
-        (sorin-ionescu prezto)
-        (pkgng         pkgng)
-        (coderholic    pyradio)
-        (bmizerany     roundup)
-        (muennich      sxiv)
-        (alice0775     userChrome.js)
-        (Griever       userChromeJS)
-        (rkitover      vimpager)
-        (GGLucas       vimperator-buftabs)
-        (vimpr         vimperator-plugins)
-        (caisui        vimperator)
-        (clvv           fasd)
-        (rupa           z)
-        (rupa           v)
-        (sjl            z-fish)
-        (zsh-users     zsh-completions)
-        (zsh-users     zsh-syntax-highlighting)
-        (trapd00r      zsh-syntax-highlighting-filetypes)
-        (trapd00r      ls--)
-        (trapd00r      utils trapd00r-utils)
-        (trapd00r      configs trapd00r-configs)
-        (hchbaw        auto-fu.zsh)
-        (buntine       Fractals)
-        (SanskritFritz fish_completions)
-        (esodax        fishystuff)
-        (zmalltalker        fish-nuggets)
-        (Nandaka       DanbooruDownloader)
-        (frytvm        XS)
-        (joelagnel     stumpwm-goodies)
-        (sabetts       stumpwm)
-        (aredridel      es-shell)
-        (mooz           percol)
-        (xorg62         wmfs)
-        (shawncplus     ghub)
-        (86me           pentadactyl-scripts)
-        (lf94           dwm-lee)
-        (Cloudef        monsterwm-xcb)
-        (Cloudef        dotFiles   cloudef-dotFiles)
-        (Cloudef        dotfiles-ng   cloudef-dotFiles-ng)
-        (DaisukeAramaki Dotfiles)
-        (chjj           compton)
-        (samirahmed           fu)
-        (defunkt        hub)
-        (huyz           less.vim)
-        (ivmai          bdwgc)
-        (ivmai          libatomic_ops)
-        (gmarik         vimfiles gmarik-vimfiles)
-        (valvallow      lifegame)
-        (saironiq       shellscripts)
-        (mason-larobina luakit)
-        (robm           dzen)
-        (jhawthorn     meh)
-        (kahua         Kahua)
-        (digego         extempore)
-        (tlatsas       xcolors)
-        (Raynes       fs)
-        (clojure      clojure-contrib)
-        (clojure      core.logic)
-        (sgrove       tehila)
-        (tyru         dotto)
-        (ThomasAdam     tmux)
-        (erikw        tmux-powerline)
-        (seebi        tmux-colors-solarized)
-        (altercation        solarized)
-        (cucumber        cucumber)
-        (cavalle        steak)
-        (cldwalker        boson-more)
-        (cldwalker        boson)
-        (webyrd        miniKanren)
-        (chujoii       battery-scheme)
-        (chujoii       xattr-tag)
-        (ghc ghc)
-        (francesco-bracchi sake)
-        (adamv          homebrew)
-        (okuoku r7rs-bridge)
-        ))
-
     ;; update git repository
     (define (do-update)
       (let ((repos (find-git-repository *gitdir*)))
@@ -168,45 +69,6 @@
                       (newline))
                      (else  #true))
                  (loop (cdr dirs)))))))
-
-    ;; clone git repository
-    (define (clone-gitdir)
-      (let ((clone (lambda (url dirname) (run-process `(git clone ,url ,dirname) :wait #true))))
-        (for-each
-            (lambda (l)
-              (if-not (file-is-directory? (x->string (car l)))
-                      (clone (cadr l) (car l))
-                      #true))
-          (repo-url-directory-list))
-        #true))
-
-    (define (repo-url-directory-list)
-      (map
-          (lambda (e)
-            (cond
-              ((string? e)
-               ;; normal repo
-               (list (sys-basename (path-sans-extension e)) e))
-              ((list? e)
-               (cond
-                 ((string? (car e))
-                                        ; normal repo
-                  (list (cadr e) (car e)))
-                 ((= (length e) 2)
-                  ;; github
-                  (list (cadr e)
-                    (string-append "git://github.com/" (car e)  "/" (cadr e))))
-                 ;; github with renaming
-                 (else
-                     (list (caddr e)
-                       (string-append "git://github.com/" (car e)  "/" (cadr e))))))
-              ((symbol? e)
-               ;; my github
-               (list e
-                 (string-append "git@github.com:mytoh/" e)))
-              (else
-                  (list e))))
-        *repos* ))
 
     (define options-list
       (list (option '(#\p "full-path") (not 'require-arg?) (not 'optional-arg?)
@@ -291,10 +153,6 @@
                                   (clean-gitdir))
                                  ("list"
                                   (do-list rest))
-                                 ("clone"
-                                  (begin
-                                    (print (string-append (paint "cloning " 3) "repositories"))
-                                    (clone-gitdir)))
                                  ("get"
                                   (do-get (cadr rest)))
                                  (_ (usage 1)))))
