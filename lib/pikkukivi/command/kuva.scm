feafa68bc6b12fa5647fd626b6b343851c3b29e3
mytoh <mytoh@gmail.com>
2014-08-20 20:55:56 +0900

    * lib/pikkukivi/command/kuva.scm: use sxiv

--- lib/pikkukivi/command/kuva.scm
+++ lib/pikkukivi/command/kuva.scm
@@ -1,5 +1,5 @@
 
-(define-library (pikkukivi command kuva)
+ (define-library (pikkukivi command kuva)
     (export
       kuva)
   (import
@@ -17,18 +17,35 @@
   (begin
     (define (usage status) (exit status "usage: ~a <file>\n" "kuva"))
 
-    (define default-feh-options
+    (define feh-default-options
       '(--auto-zoom --fullscreen --quiet --reverse --sort mtime))
 
-    (define (open-directory dir)
-      (run-process `(feh ,@default-feh-options ,dir) :wait #true))
+    (define sxiv-directory-options
+      '(-s f -f -b -a -r))
 
-    (define (open-regular-file file)
-      (run-process `(feh ,@default-feh-options
+    (define (open-directory-feh dir)
+      (run-process `(feh ,@feh-default-options ,dir) :wait #true))
+
+    (define (open-directory-sxiv dir)
+      (run-process `(sxiv ,@sxiv-directory-options ,dir)
+                   :wait #true))
+
+    (define (open-directory dir)
+      (open-directory-sxiv dir))
+
+    (define (open-regular-file-feh file)
+      (run-process `(feh ,@feh-default-options
                          --start-at
                          ,(sys-realpath file)
                          ,(sys-dirname (sys-realpath file)))
                    :wait #true))
+
+    (define (open-regular-file-sxiv file)
+      (run-process `(sxiv ,@sxiv-directory-options ".")
+                   :wait #true))
+
+    (define (open-regular-file file)
+      (open-regular-file-sxiv file))
 
     (define (open-archive file)
       (let ((temp (build-path
@@ -39,7 +56,7 @@
                      (path-sans-extension file))))))
         (make-directory* temp)
         (unpack (list file temp))
-        (run-process `(feh ,@default-feh-options  -r ,temp) :wait #true)
+        (run-process `(feh ,@feh-default-options  -r ,temp) :wait #true)
         (remove-directory* temp)))
 
     (define options
@@ -47,32 +64,31 @@
                     (lambda (option name arg help)
                       (values #true)))))
 
-    (define open
-      (lambda (args)
-        (cond ((null? args)
-               (run-process `(feh) :wait #true))
-              (else
-                  (receive (help)
-                    (args-fold args
-                      options
-                      (lambda (option name arg . seeds)
-                        (error "Unrecognized option:" name))
-                      (lambda (operand help)
-                        (values help))
-                      #false ; default help
-                      )
-                    (cond
-                      (help
-                       (usage 0))
-                      (else
-                          (match (car args)
-                                 ((? file-is-directory? dir)
-                                  (open-directory dir))
-                                 ((? file-is-archive? file)
-                                  (open-archive file))
-                                 ((? file-is-regular? file)
-                                  (open-regular-file file))
-                                 (_ (usage 1))))))))))
+    (define (open args)
+      (cond ((null? args)
+             (open-directory "."))
+            (else
+                (receive (help)
+                  (args-fold args
+                    options
+                    (lambda (option name arg . seeds)
+                      (error "Unrecognized option:" name))
+                    (lambda (operand help)
+                      (values help))
+                    #false ; default help
+                    )
+                  (cond
+                    (help
+                     (usage 0))
+                    (else
+                        (match (car args)
+                               ((? file-is-directory? dir)
+                                (open-directory dir))
+                               ((? file-is-archive? file)
+                                (open-archive file))
+                               ((? file-is-regular? file)
+                                (open-regular-file file))
+                               (_ (usage 1)))))))))
 
     (define (kuva args)
       (open args))
